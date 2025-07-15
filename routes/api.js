de3286eb750f92ccf2897db66c742d6ba85d9c27
Ankitkyada <ankitkyada2002@gmail.com>
2025-07-15 22:22:57 +0530

    auth middelware based change

--- routes/api.js
+++ routes/api.js
@@ -5,15 +5,15 @@ const router = express.Router();
 const AuthController = require('../app/Controllers/AuthController');
 const UserController = require('../app/Controllers/UserController');
 const AuditLogsController = require('../app/Controllers/AuditLogsController');
-const authMiddleware = require('../app/Middleware/Auth');
+
 
 // Public routes
 router.post('/login', AuthController.login);
 
 
 router.get('/get-users-list', UserController.getUsers);
-router.get('/get-user/:id', authMiddleware, UserController.getUserById);
-router.post('/add-user', authMiddleware, UserController.addUser);
+router.get('/get-user/:id', UserController.getUserById);
+router.post('/add-user', UserController.addUser);
 
 
 router.get('/audit_logs', AuditLogsController.getAuditLogs);
